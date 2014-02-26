19c9afa92a8a499e1e9267dc76b8a1e1aff90245
Irsael viveros <jose.viveros@esmas.net>
2014-02-25 19:44:47 -0600

    implementación del contador
    mejoras css y js

---
+++ widgets/wdg_contador_header/js/TickerFutbol_99jsonp.js
@@ -0,0 +1,426 @@
+wtdata({
+	"matches": {
+		"timeNow": 1391683745,
+		"match": [{
+			"time": null,
+			"MatchHour2": "08:00:00",
+			"MXvideo": "",
+			"MatchGuid": "6f9516c0-3226-1030-8a0b-0019b9d72a1e",
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012/17229/brasil-vs-mexico/",
+			"USAvideo": "",
+			"EventTournamentId": "128",
+			"MatchDate": "24/02/2014",
+			"TimeStamp": 1344675600,
+			"period": "F",
+			"periodabrev": "FIN..",
+			"EventId": "9422",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "M\u00e9xico",
+					"club": "M\u00e9xico",
+					"guid": "18805b74-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Brasil",
+					"club": "Confedera\u00e7\u00e3o Brasileira de Futebol",
+					"guid": "fe09e08a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "1",
+					"result": "PERDIDO"
+				}
+			},
+			"MatchId": "17229",
+			"EventUrl": "http://tv.televisadeportes.esmas.com/evento/futbol/128/9422/juegos-olimpicos-londres-2012/brasil-vs-mexico",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchHour": "18:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012",
+			"extras":''
+		}, {
+			"time": null,
+			"MatchHour2": "13:45",
+			"MXvideo": "",
+			"MatchGuid": "a9d0c1ac-3228-1030-8a0b-0019b9d72a1e",
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012/17230/corea-del-sur-vs-japon/",
+			"USAvideo": "",
+			"EventTournamentId": "128",
+			"MatchDate": "10/08/2012",
+			"TimeStamp": 1344606300,
+			"period": "F",
+			"periodabrev": "FIN",
+			"EventId": "9423",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Jap\u00f3n",
+					"club": "Nihon Koku",
+					"guid": "febba856-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Corea del Sur",
+					"club": "Daehanminguk",
+					"guid": "fe7b175a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"MatchId": "17230",
+			"EventUrl": "http://tv.televisadeportes.esmas.com/evento/futbol/128/9423/juegos-olimpicos-londres-2012/corea-del-sur-vs-japon",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchHour": "13:45:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012",
+			"extras":''
+		}, {
+			"TimeStamp": 1344519900,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:45",
+			"periodabrev": "FIN",
+			"MXvideo": "",
+			"MatchGuid": "0772b18c-3164-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Jap\u00f3n",
+					"club": "Nihon Koku",
+					"guid": "febba856-a105-102c-8c37-0019b9d72a1e",
+					"goals": "1",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Estados Unidos",
+					"club": "United States of America",
+					"guid": "ff2caf10-a105-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17226/estados-unidos-vs-japon/",
+			"USAvideo": "",
+			"MatchId": "17226",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "11:45:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":'final'
+		}
+		, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}
+		, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "México",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Camerún",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":'revive'
+		}
+		, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}
+		, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}
+		, {
+			"TimeStamp": 1344495600,
+			"period": "F",
+			"time": null,
+			"MatchHour2": "13:00",
+			"periodabrev": "FIN..",
+			"MXvideo": "",
+			"MatchGuid": "0dc6682c-3163-1030-8a0b-0019b9d72a1e",
+			"equipos": {
+				"visit": {
+					"penals": "0",
+					"name": "Francia",
+					"club": "F\u00e9d\u00e9ration Fran\u00e7aise de Football",
+					"guid": "fcca290a-a105-102c-8c37-0019b9d72a1e",
+					"goals": "0",
+					"result": "PERDIDO"
+				},
+				"local": {
+					"penals": "0",
+					"name": "Canad\u00e1",
+					"club": "Canada",
+					"guid": "253b88f2-a106-102c-8c37-0019b9d72a1e",
+					"goals": "2",
+					"result": "GANADO"
+				}
+			},
+			"Website": "http://mxm.televisadeportes.esmas.com/futbol/partidos/juegos-olimpicos-londres-2012--femenil/17225/canada-vs-francia/",
+			"USAvideo": "",
+			"MatchId": "17225",
+			"txtLink": "Cr&oacute;nica &raquo;",
+			"MatchDate": "06/02/2014",
+			"MatchHour": "12:00:00",
+			"EventTournamentName": "Juegos Ol\u00edmpicos Londres 2012. Femenil",
+			"extras":''
+		}
+		]
+	}
+})
